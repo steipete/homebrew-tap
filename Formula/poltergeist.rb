8cdd1a78b71f7b28ed060407241429ae84a9b193
Peter Steinberger <steipete@gmail.com>
2025-08-07 13:04:58 +0200

    Update Poltergeist to v1.6.0 - Bun binary release

--- Formula/poltergeist.rb
+++ Formula/poltergeist.rb
@@ -1,36 +1,22 @@
-require "language/node"
-
 class Poltergeist < Formula
   desc "Universal file watcher with auto-rebuild for any language or build system"
   homepage "https://github.com/steipete/poltergeist"
-  url "https://registry.npmjs.org/@steipete/poltergeist/-/poltergeist-1.6.0.tgz"
-  sha256 "dda0cfd2ba8f4e513dfc59bb207d1b501140d72209898e619d9c2cfd7423a0ea"
+  url "https://github.com/steipete/poltergeist/releases/download/v1.6.0/poltergeist-macos-universal-v1.6.0.tar.gz"
+  sha256 "48704c9237e1c48576823e6ac251fc49748adc50d195b12ea6f6ab0f8477cd1b"
   license "MIT"
+  version "1.6.0"
 
-  depends_on "node"
   depends_on "watchman"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    
-    # Create wrapper scripts
-    bin.install_symlink Dir["#{libexec}/bin/*"]
-    
-    # Install polter as a separate command
-    (bin/"polter").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/lib/node_modules/@steipete/poltergeist/dist/polter.js" "$@"
-    EOS
-    
-    # Make poltergeist the main command
-    (bin/"poltergeist").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/lib/node_modules/@steipete/poltergeist/dist/cli.js" "$@"
-    EOS
-    
-    # Ensure scripts are executable
-    chmod 0755, bin/"polter"
-    chmod 0755, bin/"poltergeist"
+    bin.install "poltergeist"
+    bin.install "polter"
+  end
+
+  def post_install
+    # Ensure binaries are executable
+    chmod 0755, "#{bin}/poltergeist"
+    chmod 0755, "#{bin}/polter"
   end
 
   def caveats
@@ -45,13 +31,18 @@ class Poltergeist < Formula
         3. Run 'poltergeist start' to begin watching
         4. Use 'polter <target>' to run your binaries
 
+      Watchman is required and has been installed as a dependency.
+      
       Documentation: https://github.com/steipete/poltergeist
     EOS
   end
 
   test do
-    system "#{bin}/poltergeist", "--version"
-    system "#{bin}/polter", "--help"
+    # Test that the binary runs and returns version
+    assert_match "1.6.0", shell_output("#{bin}/poltergeist --version")
+    
+    # Test polter wrapper
+    assert_match "Poltergeist", shell_output("#{bin}/polter --help")
     
     # Test that watchman dependency is available
     assert_match "version", shell_output("watchman version")
