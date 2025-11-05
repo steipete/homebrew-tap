f267daee1ea0418f95e38ef38f41ee5a25a96c32
Peter Steinberger <steipete@gmail.com>
2025-11-05 09:11:36 +0000

    Update tmuxwatch formula to 0.9.1

--- Formula/tmuxwatch.rb
+++ Formula/tmuxwatch.rb
@@ -1,8 +1,8 @@
 class Tmuxwatch < Formula
   desc "Live tmux dashboard with Bubble Tea UI"
   homepage "https://github.com/steipete/tmuxwatch"
-  url "https://github.com/steipete/tmuxwatch/archive/refs/tags/v0.9.tar.gz"
-  sha256 "232facf5ddf0b12347bf0e45d0428e95cf76fabc107930d98817382dbce6797d"
+  url "https://github.com/steipete/tmuxwatch/archive/refs/tags/v0.9.1.tar.gz"
+  sha256 "75650cbbf0f9528125f62525bc3b39f3a53d12ef0f57674f68b2b30dc537c0cf"
   license "MIT"
 
   depends_on "go" => :build
@@ -13,6 +13,6 @@ class Tmuxwatch < Formula
   end
 
   test do
-    assert_match "tmuxwatch 0.9", shell_output("#{bin}/tmuxwatch --version")
+    assert_match "tmuxwatch 0.9.1", shell_output("#{bin}/tmuxwatch --version")
   end
 end
