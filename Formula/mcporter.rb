ff732b005fd6364c721e0dc284d6604af4ce458d
Peter Steinberger <steipete@gmail.com>
2025-11-07 23:41:27 +0000

    mcporter 0.3.3

--- Formula/mcporter.rb
+++ Formula/mcporter.rb
@@ -2,7 +2,7 @@ class Mcporter < Formula
   desc "Model Context Protocol runtime and CLI generator"
   homepage "https://github.com/steipete/mcporter"
   url "https://github.com/steipete/mcporter/releases/download/v0.3.3/mcporter-macos-arm64-v0.3.3.tar.gz"
-  sha256 "efedbabf77c736bf99834ace099657f58e267dc548c6649f1550e9f53b77aa53"
+  sha256 "8f01ba0683e220f3965c1af484f412bc09447632984ad2c4485325060817d821"
   license "MIT"
   version "0.3.3"
 
