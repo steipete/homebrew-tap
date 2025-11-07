fb7dfe337d0bba02813e862fefef4b54dc46bbc2
Peter Steinberger <steipete@gmail.com>
2025-11-07 23:32:29 +0000

    chore(mcporter): update 0.3.3 checksum

--- Formula/mcporter.rb
+++ Formula/mcporter.rb
@@ -2,7 +2,7 @@ class Mcporter < Formula
   desc "Model Context Protocol runtime and CLI generator"
   homepage "https://github.com/steipete/mcporter"
   url "https://github.com/steipete/mcporter/releases/download/v0.3.3/mcporter-macos-arm64-v0.3.3.tar.gz"
-  sha256 "bfb21fa81744dafdac5fec7322695839f09e277925038743cbfa321550c2f235"
+  sha256 "efedbabf77c736bf99834ace099657f58e267dc548c6649f1550e9f53b77aa53"
   license "MIT"
   version "0.3.3"
 
