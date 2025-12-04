4e705a83dc3b35a12518623cade5ea4ffbe409c3
Peter Steinberger <steipete@gmail.com>
2025-12-04 15:35:16 +0100

    Point camsnap formula to v0.2.0 tag

--- Formula/camsnap.rb
+++ Formula/camsnap.rb
@@ -3,7 +3,7 @@ class Camsnap < Formula
   homepage "https://github.com/steipete/camsnap"
   url "https://github.com/steipete/camsnap.git",
       tag: "v0.2.0",
-      revision: "894cb41fe195437fa3702cd32be0ed137ba5ab41"
+      revision: "141ca9f83b8a6ae9e8fb9a40c2a1dc040bfee1c8"
   license "MIT"
 
   depends_on "go" => :build
