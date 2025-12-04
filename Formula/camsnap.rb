4c00fbf1df165142d0cff9b2aa08451f6885e8a6
Peter Steinberger <steipete@gmail.com>
2025-12-04 15:31:59 +0100

    Add camsnap 0.2.0 formula

---
+++ Formula/camsnap.rb
@@ -0,0 +1,20 @@
+class Camsnap < Formula
+  desc "One command to grab frames, clips, or motion alerts from RTSP/ONVIF cams"
+  homepage "https://github.com/steipete/camsnap"
+  url "https://github.com/steipete/camsnap.git",
+      tag: "v0.2.0",
+      revision: "edb5770fee2e204b799b9580def1e92bac7e27d5"
+  license "MIT"
+
+  depends_on "go" => :build
+  depends_on "ffmpeg"
+
+  def install
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args(ldflags:, output: bin/"camsnap"), "./cmd/camsnap"
+  end
+
+  test do
+    assert_match version, shell_output("#{bin}/camsnap --version")
+  end
+end
