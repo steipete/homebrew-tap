2e610b74652bfa589723bad8271a2bb992a42ce3
Peter Steinberger <steipete@gmail.com>
2025-12-08 17:56:46 +0100

    sag: update v0.1.0 sha

--- Formula/sag.rb
+++ Formula/sag.rb
@@ -2,7 +2,7 @@ class Sag < Formula
   desc "Command-line ElevenLabs TTS with mac-style flags"
   homepage "https://github.com/steipete/sag"
   url "https://github.com/steipete/sag/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "4cde050c7a4294b3c87c6e6cd4e7b219e377b26a5ae51490121b7bb18242c245"
+  sha256 "46df20553619e122677e24408cc2ae1902ae6674d9da49db268a99b416eb6a4f"
   license "MIT"
   version "0.1.0"
 
