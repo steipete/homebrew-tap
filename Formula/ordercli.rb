2ef3f05cf3612870ac9514927e99c79f38753edf
Peter Steinberger <steipete@gmail.com>
2025-12-20 18:56:04 +0100

    ordercli v0.1.0

--- Formula/ordercli.rb
+++ Formula/ordercli.rb
@@ -2,7 +2,7 @@ class Ordercli < Formula
   desc "Multi-provider order CLI (Foodora, Deliveroo)"
   homepage "https://github.com/steipete/ordercli"
   url "https://github.com/steipete/ordercli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f455751ed946465e884dee8b8330019ca9b734fc23b4d0fdd3a9217156d72656"
+  sha256 "03c06c099f21c4a9819e2804522903d84ea8a52044f119ce9846c78243ffd68f"
   version "0.1.0"
   license "MIT"
 
