e237a3723b1e6e58420ad11e5caa3a5c722a2fdf
Peter Steinberger <steipete@gmail.com>
2025-12-20 13:26:23 +0100

    chore: bump summarize to 0.2.0

--- Formula/summarize.rb
+++ Formula/summarize.rb
@@ -1,10 +1,10 @@
 class Summarize < Formula
   desc "Link → clean text → summary"
   homepage "https://github.com/steipete/summarize"
-  url "https://github.com/steipete/summarize/releases/download/v0.1.2/summarize-macos-universal-v0.1.2.tar.gz"
-  sha256 "584cd2c6078dde02f9aa616e7f6bd198c8b676c370a7d88bf14084119f62e45a"
+  url "https://github.com/steipete/summarize/releases/download/v0.2.0/summarize-macos-universal-v0.2.0.tar.gz"
+  sha256 "c0847d74831e68ba7ae7903e3b509e0ae75821a26b362529816dd2b500482828"
   license "MIT"
-  version "0.1.2"
+  version "0.2.0"
 
   def install
     bin.install "summarize"
@@ -15,7 +15,7 @@ class Summarize < Formula
   end
 
   test do
-    assert_match "0.1.2", shell_output("#{bin}/summarize --version")
+    assert_match "0.2.0", shell_output("#{bin}/summarize --version")
     assert_match "Summarize web pages", shell_output("#{bin}/summarize --help")
   end
 end
