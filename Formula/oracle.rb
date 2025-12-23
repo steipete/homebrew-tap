625dbe1f763aa215ecd675189635b3f0d7efe2d0
Peter Steinberger <steipete@gmail.com>
2025-12-23 20:56:56 +0100

    chore: update oracle to 0.7.5

--- Formula/oracle.rb
+++ Formula/oracle.rb
@@ -3,15 +3,16 @@ require "language/node"
 class Oracle < Formula
   desc "Bundle prompts + files for second-model review"
   homepage "https://github.com/steipete/oracle"
-  url "https://github.com/steipete/oracle/releases/download/v0.7.4/oracle-0.7.4.tgz"
-  sha256 "bf72e4bad190e5c71bcd18aceee28c21c9758c0f3334f907531809858a330a35"
+  url "https://github.com/steipete/oracle/releases/download/v0.7.5/oracle-0.7.5.tgz"
+  sha256 "df383458a18b78804cf90c22ec1f831778c80b47f287ce19c47a71581833ce5d"
   license "MIT"
-  version "0.7.4"
+  version "0.7.5"
 
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec, ignore_scripts: false)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
