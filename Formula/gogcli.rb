53541c1671d4483fd4437ea35f8f4009b111f3d1
Peter Steinberger <steipete@gmail.com>
2025-12-24 22:33:12 +0100

    gogcli v0.2.0

--- Formula/gogcli.rb
+++ Formula/gogcli.rb
@@ -1,10 +1,10 @@
 class Gogcli < Formula
   desc "Google CLI for Gmail, Calendar, Drive, and Contacts"
   homepage "https://github.com/steipete/gogcli"
-  url "https://github.com/steipete/gogcli/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "5ed777e0969a81e6208d881816a6a7f68eb76c7c20af77f31cdb33f8926e8da4"
+  url "https://github.com/steipete/gogcli/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "4d890e7fce40a6a95b3b4be9ffbca3e00b9b21c5b5ce3a163f6fa461005ec4c1"
   license "MIT"
-  version "0.1.1"
+  version "0.2.0"
 
   depends_on "go" => :build
 
