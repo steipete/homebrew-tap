fc4260988c80a90ac3519dbf20bfd79343dad11e
Peter Steinberger <steipete@gmail.com>
2025-12-25 04:10:51 +0100

    chore: bump codexbar to 0.14.0

--- Casks/codexbar.rb
+++ Casks/codexbar.rb
@@ -1,6 +1,6 @@
 cask "codexbar" do
-  version "0.13.0"
-  sha256 "e638ff0d73e9abfaf8f48b84027a0984f51ed2cff24c1563b6f8b617a06c4e39"
+  version "0.14.0"
+  sha256 "e41e31c81b79bcbc34bea8879432631b2f7a347be84f16be5015dbf5fd1c877e"
 
   url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBar-#{version}.zip",
       verified: "github.com/steipete/CodexBar/"
