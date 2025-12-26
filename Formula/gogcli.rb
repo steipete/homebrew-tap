1c753b54c4afc9a693e4255bb3f02404e56cd535
Peter Steinberger <steipete@gmail.com>
2025-12-26 10:39:36 +0000

    chore(brew): bump gogcli to 0.3.0

--- Formula/gogcli.rb
+++ Formula/gogcli.rb
@@ -1,18 +1,19 @@
 class Gogcli < Formula
   desc "Google CLI for Gmail, Calendar, Drive, and Contacts"
   homepage "https://github.com/steipete/gogcli"
-  url "https://github.com/steipete/gogcli/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "47844aa25f1c4992457d3919c521f2c82e0aacdd45f57a3c2beaf60b80cea450"
+  url "https://github.com/steipete/gogcli/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "a5155b28e00fd58e9a917b6785ca6186810180af52a1a8da0a4a85faaf605280"
   license "MIT"
-  version "0.2.1"
+  version "0.3.0"
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(output: bin/"gog", ldflags: "-s -w"), "./cmd/gog"
+    ldflags = "-s -w -X github.com/steipete/gogcli/internal/cmd.version=v#{version}"
+    system "go", "build", *std_go_args(output: bin/"gog", ldflags:), "./cmd/gog"
   end
 
   test do
-    assert_match "Google CLI", shell_output("#{bin}/gog --help")
+    assert_match "v#{version}", shell_output("#{bin}/gog --version")
   end
 end
