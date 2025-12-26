b0e23db9342a65ef2d35900002636f8e1bab9678
Peter Steinberger <steipete@gmail.com>
2025-12-26 22:46:24 +0100

    bird 0.4.0

--- Formula/bird.rb
+++ Formula/bird.rb
@@ -1,10 +1,10 @@
 class Bird < Formula
   desc "Fast X CLI for tweeting, replying, and reading"
   homepage "https://github.com/steipete/bird"
-  url "https://github.com/steipete/bird/releases/download/v0.3.0/bird-macos-universal-v0.3.0.tar.gz"
-  sha256 "8b64ce93f32562ce823cef0f906d8104c00a39655766ebc441d2a0da7830b7b1"
+  url "https://github.com/steipete/bird/releases/download/v0.4.0/bird-macos-universal-v0.4.0.tar.gz"
+  sha256 "51f82698414b14cc2237ec3fcdcfa4f39df3e1ff4476f6b2190bc073c6bc6365"
   license "MIT"
-  version "0.3.0"
+  version "0.4.0"
 
   def install
     bin.install "bird"
@@ -13,6 +13,7 @@ class Bird < Formula
   def caveats
     <<~EOS
       bird uses X/Twitter GraphQL with local cookies by default.
+      This is an undocumented/private API and can break whenever X changes things.
 
       Quick start:
         bird whoami
@@ -21,6 +22,6 @@ class Bird < Formula
   end
 
   test do
-    assert_match "0.3.0", shell_output("#{bin}/bird --version")
+    assert_match "0.4.0", shell_output("#{bin}/bird --version")
   end
 end
