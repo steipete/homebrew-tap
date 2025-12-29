07d515a88f27cec6bd8d9239d5697f5a909ff89b
Peter Steinberger <steipete@gmail.com>
2025-12-29 19:16:28 +0100

    chore: update codexbar cask

--- Casks/codexbar.rb
+++ Casks/codexbar.rb
@@ -1,6 +1,6 @@
 cask "codexbar" do
-  version "0.15.1"
-  sha256 "5508c4e5aaa1045eacd5ff9efba014cef32dece84b7f27e5ef8c53e312de2c09"
+  version "0.16.0"
+  sha256 "fd2a60744e59e27b53f1a2454ba9c4cc11241d0c0ca542d6297e7c4988c38309"
 
   url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBar-#{version}.zip",
       verified: "github.com/steipete/CodexBar/"
