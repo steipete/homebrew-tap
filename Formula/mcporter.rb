e41d09eec087bcb14177819af5d361a303b589d8
Peter Steinberger <steipete@gmail.com>
2025-12-30 00:37:31 +0100

    chore(mcporter): refresh 0.7.3 bottle sha

--- Formula/mcporter.rb
+++ Formula/mcporter.rb
@@ -2,7 +2,7 @@ class Mcporter < Formula
   desc "Model Context Protocol runtime and CLI generator"
   homepage "https://github.com/steipete/mcporter"
   url "https://github.com/steipete/mcporter/releases/download/v0.7.3/mcporter-macos-arm64-v0.7.3.tar.gz"
-  sha256 "d8b71971e574bf86b7e24f3fd6db1e13f8c89dbe8704a8b78b806b6e2a628af9"
+  sha256 "852da8adefeb78df53705aa3af12fe725067c8014a5a57d1d6a3b273d29a7dfe"
   license "MIT"
 
   depends_on arch: :arm64
