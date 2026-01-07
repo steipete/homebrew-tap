02c6eb0e5589906e3b3e3b7d587ca17ecf064e74
Peter Steinberger <steipete@gmail.com>
2026-01-07 11:07:27 +0100

    chore: bump imsg to 0.4.0

--- Formula/imsg.rb
+++ Formula/imsg.rb
@@ -1,16 +1,15 @@
 class Imsg < Formula
   desc "Send and read iMessage / SMS from the terminal"
   homepage "https://github.com/steipete/imsg"
-  url "https://github.com/steipete/imsg/releases/download/v0.2.0/imsg-macos.zip"
-  version "0.2.0"
-  sha256 "11193311bd13d9747bbd320c733e9101e0b0617d104318ec3a9f5030c38b5767"
+  url "https://github.com/steipete/imsg/releases/download/v0.4.0/imsg-macos.zip"
+  version "0.4.0"
+  sha256 "d0e5e333ee88192d595bfed9eece60e35ecad0300145966d5ad27458c33e407b"
   license "MIT"
 
   # macOS Sonoma (14.0) or later required
   depends_on macos: :sonoma
 
   def install
-    odie "imsg is Apple Silicon only (arm64)." if Hardware::CPU.intel?
     bin.install "imsg"
     bin.install "PhoneNumberKit_PhoneNumberKit.bundle"
   end
