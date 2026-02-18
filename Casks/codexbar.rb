f9077153493951db8abef18b469fdbc57cbd55a8
Mohamad Khajavi <khajavi.contact@gmail.com>
2026-02-19 01:28:32 +0330

    chore: update codexbar to 0.18.0-beta.3

--- Casks/codexbar.rb
+++ Casks/codexbar.rb
@@ -1,6 +1,6 @@
 cask "codexbar" do
-  version "0.17.0"
-  sha256 "2c3f3a49008f2e2ff264e4a36ad027b5c140d61c331ac2e7ace2c1ff9b0c69dc"
+  version "0.18.0-beta.3"
+  sha256 "3806e9b8c1c6c12b8c7d2e60b6bcaff3e677bc39ec40dade8fc4ee9de96568fd"
 
   url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBar-#{version}.zip",
       verified: "github.com/steipete/CodexBar/"
