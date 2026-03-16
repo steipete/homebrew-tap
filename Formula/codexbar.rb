782e279da0b67bba5729413c839445db059ca7f1
Peter Steinberger <steipete@gmail.com>
2026-03-15 22:21:43 -0700

    chore: release codexbar 0.18.0

--- Formula/codexbar.rb
+++ Formula/codexbar.rb
@@ -1,16 +1,16 @@
 class Codexbar < Formula
   desc "CodexBar CLI for usage/status output"
   homepage "https://github.com/steipete/CodexBar"
-  version "0.17.0"
+  version "0.18.0"
   license "MIT"
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBarCLI-v#{version}-linux-aarch64.tar.gz"
-      sha256 "7b86ee04473b85108773ade40127f723e1811d38a85eefcab2bb2a06728cd006"
+      sha256 "601fc75bd911ebb5ce3acc863e534a4e04b000398e6851af3e18978e4a81792b"
     else
       url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBarCLI-v#{version}-linux-x86_64.tar.gz"
-      sha256 "9bf9bb6fbd33ed1a3c23767e9dad8801318f6722e86aaa0437400a37c12bfbdd"
+      sha256 "622f26303591a5901604fa5b1bab155149e6f7f99c5084b360608d714c5b0642"
     end
   end
 
