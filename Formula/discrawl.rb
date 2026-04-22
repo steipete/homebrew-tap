229e2cf49312be7c376175585b33a7b5eb0fdf56
Peter Steinberger <steipete@gmail.com>
2026-04-22 18:43:11 +0100

    chore: update discrawl formula to 0.4.1

--- Formula/discrawl.rb
+++ Formula/discrawl.rb
@@ -1,26 +1,26 @@
 class Discrawl < Formula
   desc "Discord archive CLI for local SQLite search and analysis"
   homepage "https://github.com/steipete/discrawl"
-  version "0.4.0"
+  version "0.4.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/steipete/discrawl/releases/download/v#{version}/discrawl_#{version}_darwin_arm64.tar.gz"
-      sha256 "f88fb0ec43a14acb57947ea612923583c0a62467779ca5d24844daa1568fe87b"
+      sha256 "27f441c5871e9bbfc3337974d703a46a644b50969654dd7f9e361596935e65a0"
     else
       url "https://github.com/steipete/discrawl/releases/download/v#{version}/discrawl_#{version}_darwin_amd64.tar.gz"
-      sha256 "8eeefbd4f3a32917f2faedf7135835475acb57fae395436e45a4e1f97abb9f54"
+      sha256 "dfac3319a11a8e059d6f908281628f5e7b976fd522584f89c667d12ad7d4d790"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/steipete/discrawl/releases/download/v#{version}/discrawl_#{version}_linux_arm64.tar.gz"
-      sha256 "880be2ccb7f1c9e999d216dca085c42f36844bed4c493d38b881cce925d8f333"
+      sha256 "88e6840a8a99c7597d16a478a58bc4d11c9db905495d6b0e1e5ba80cc4e7e865"
     else
       url "https://github.com/steipete/discrawl/releases/download/v#{version}/discrawl_#{version}_linux_amd64.tar.gz"
-      sha256 "7a0fda07cf355d952105d8328061a0972bca559263b1b5118df5f4493333c5a9"
+      sha256 "a136ada04115750b6cd0a0f812b347f34e32f7973898d72a9b84a440213e2627"
     end
   end
 
