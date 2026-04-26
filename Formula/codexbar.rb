fbf9c3963dfe04ce4e34549c5b486d8215668721
Peter Steinberger <steipete@gmail.com>
2026-04-26 04:59:45 +0100

    chore: update CodexBar to 0.23

--- Formula/codexbar.rb
+++ Formula/codexbar.rb
@@ -1,9 +1,9 @@
 class Codexbar < Formula
   desc "Menu bar usage and status CLI"
   homepage "https://github.com/steipete/CodexBar"
-  url "https://github.com/steipete/CodexBar/releases/download/v0.22/CodexBarCLI-v0.22-linux-x86_64.tar.gz"
-  version "0.22"
-  sha256 "f330406c421eef7e9a0b199f0f1baeced12cffa206cbaf3be45f5d00def3d217"
+  url "https://github.com/steipete/CodexBar/releases/download/v0.23/CodexBarCLI-v0.23-linux-x86_64.tar.gz"
+  version "0.23"
+  sha256 "710c2697672516d7bec15e51b93a6a7bfb8de3056bc0690ad69d5ba6a6ece4e9"
   license "MIT"
 
   depends_on :linux
@@ -11,10 +11,10 @@ class Codexbar < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBarCLI-v#{version}-linux-aarch64.tar.gz"
-      sha256 "9f72e70ef5af68748bbe00570dd82dc562f5856607c98e949c0846a0e13ed381"
+      sha256 "f79e00b911bfb6d0f629c40b4d81c930d590152883d6b7ee415fb9c7060b4770"
     else
       url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBarCLI-v#{version}-linux-x86_64.tar.gz"
-      sha256 "f330406c421eef7e9a0b199f0f1baeced12cffa206cbaf3be45f5d00def3d217"
+      sha256 "710c2697672516d7bec15e51b93a6a7bfb8de3056bc0690ad69d5ba6a6ece4e9"
     end
   end
 
