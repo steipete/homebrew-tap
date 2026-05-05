670ce644ef4c27c5ede3f15827601cd6592c5244
Peter Steinberger <steipete@gmail.com>
2026-05-05 08:17:11 +0100

    brew: update spogo to 0.3.0

--- Formula/spogo.rb
+++ Formula/spogo.rb
@@ -1,26 +1,26 @@
 class Spogo < Formula
   desc "Spotify power CLI using web cookies"
   homepage "https://github.com/steipete/spogo"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/steipete/spogo/releases/download/v#{version}/spogo_#{version}_darwin_arm64.tar.gz"
-      sha256 "4ff7edd6c820c8c444d5a9190825366af47210190073f000d7817e666eada32b"
+      sha256 "31347589b270d44522eef1ebb7cb017f20553280661bd857407a433068e17a37"
     else
       url "https://github.com/steipete/spogo/releases/download/v#{version}/spogo_#{version}_darwin_amd64.tar.gz"
-      sha256 "4078c4150c50e15d529ee184568ebc98da160bf3ea34f9d6aa29d9ff17da5634"
+      sha256 "7cd989b5656d1c0b8151a856f7a282c94524454d2eba5081fc328bd2bed7faaa"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/steipete/spogo/releases/download/v#{version}/spogo_#{version}_linux_arm64.tar.gz"
-      sha256 "7c48e7a70cc69a6b5e5d29eba559e235e15f39499d87b0bf7bcebf47cb705a5f"
+      sha256 "a8ddc30c071d7d1ed53457a40cdfa401aa31a1fc55c2f7976ad75918c5e28197"
     else
       url "https://github.com/steipete/spogo/releases/download/v#{version}/spogo_#{version}_linux_amd64.tar.gz"
-      sha256 "5e57e0869858b6b4fe36d8553ccee4424685ef84f1ad5e9fc771086f2ffe9947"
+      sha256 "aa22053b33d322d7d35fc7bc6bc1268f926a076468cadd6265344062d05a1f84"
     end
   end
 
