e49b8b891e14c9f7b8982a2513be96e7435c1edf
Peter Steinberger <steipete@gmail.com>
2026-05-06 05:40:12 +0100

    sag v0.3.0

--- Formula/sag.rb
+++ Formula/sag.rb
@@ -1,14 +1,14 @@
 class Sag < Formula
   desc "Command-line ElevenLabs TTS with mac-style flags"
   homepage "https://github.com/steipete/sag"
-  url "https://github.com/steipete/sag/releases/download/v0.2.2/sag_0.2.2_darwin_universal.tar.gz"
-  sha256 "0554baef912217d9e1f3988fb6d7492d46d2f49105a5eb9175e3f861f39cd289"
+  url "https://github.com/steipete/sag/releases/download/v0.3.0/sag_0.3.0_darwin_universal.tar.gz"
+  sha256 "e89a9f8c0d3daa506b74ba7bd8d09b93454d919ffacefe54d2b5dc6288409f14"
   license "MIT"
 
   on_linux do
     on_arm do
       url "https://github.com/steipete/sag/archive/refs/tags/v#{version}.tar.gz"
-      sha256 "82a09382d6803396e29a3fdc1d1d4982ffd97d69b5c6f9b6e31d0fb3818450db"
+      sha256 "acb6f2d626ce47640dc71c927dae27c6c104bab727dfa4f5bb14ebea80c11a63"
 
       depends_on "go" => :build
       depends_on "pkgconf" => :build
@@ -17,7 +17,7 @@ class Sag < Formula
 
     on_intel do
       url "https://github.com/steipete/sag/releases/download/v#{version}/sag_#{version}_linux_amd64.tar.gz"
-      sha256 "fddfe2553648fd9cd3446610c55176d897850d79f4aa95d60b605d387ffabdc8"
+      sha256 "8d4c23a9e3913e984c5f7c615f7eb3cbf5bf5f5d0f3f4c0fe51cb3e60829297e"
     end
   end
 
