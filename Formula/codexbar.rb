3f96eb9f0fa1e4c69d1f115b68d968f4bdbbd585
Peter Steinberger <steipete@gmail.com>
2026-05-06 03:14:31 +0100

    brew: sync CodexBar 0.24 checksums

--- Formula/codexbar.rb
+++ Formula/codexbar.rb
@@ -3,26 +3,26 @@ class Codexbar < Formula
   homepage "https://github.com/steipete/CodexBar"
   url "https://github.com/steipete/CodexBar/releases/download/v0.24/CodexBarCLI-v0.24-macos-arm64.tar.gz"
   version "0.24"
-  sha256 "f9a338cebfa8c23003528a86c03f06a22fdd3e836cf814dbf450f84cc1ab7eda"
+  sha256 "c165978b0cd81d525e726ee3964ed65903cf736d22eeaa492c9a792f859d7091"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBarCLI-v#{version}-macos-arm64.tar.gz"
-      sha256 "f9a338cebfa8c23003528a86c03f06a22fdd3e836cf814dbf450f84cc1ab7eda"
+      sha256 "c165978b0cd81d525e726ee3964ed65903cf736d22eeaa492c9a792f859d7091"
     else
       url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBarCLI-v#{version}-macos-x86_64.tar.gz"
-      sha256 "aaec5831ede5c81110b59ae743d740b0bb830fc51afe0c16aea13df51c1ec4a6"
+      sha256 "22abf4e3adffe51e5e126d0a17119e111bc18dabb51621214fb7ae32d1a201d0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBarCLI-v#{version}-linux-aarch64.tar.gz"
-      sha256 "f8bc3706435056a0d549fd9ce52a8e4f265d4956d2b07e6e66e8aa0f8c9b63c6"
+      sha256 "c633932deb413622f569a7f0b071a9a6374b1df551b59d7117afddee2e08bcb6"
     else
       url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBarCLI-v#{version}-linux-x86_64.tar.gz"
-      sha256 "d9ba937f4e9611389bb61ae21092242d5ef3329036a216d0c2ebe9ad5a979573"
+      sha256 "4939168b3d76fd5ee568fb64fda5b2104e7a9623b61e35e1d51dadc471f16b81"
     end
   end
 
