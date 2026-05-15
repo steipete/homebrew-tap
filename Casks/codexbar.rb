2f2ef2cb81cd456903010b91b8635b20199c9f31
Peter Steinberger <steipete@gmail.com>
2026-05-15 15:37:18 +0100

    Update CodexBar cask to 0.26.1

--- Casks/codexbar.rb
+++ Casks/codexbar.rb
@@ -1,6 +1,6 @@
 cask "codexbar" do
-  version "0.26.0"
-  sha256 "1c343eb1af58c4a025cfc424181d49412a94f067a14997edbd4dbe1b3c5162e2"
+  version "0.26.1"
+  sha256 "e47a793715747b373095edb488de3074d096a3e08d070628b19f223530f2996a"
 
   url "https://github.com/steipete/CodexBar/releases/download/v#{version}/CodexBar-macos-universal-#{version}.zip",
       verified: "github.com/steipete/CodexBar/"
