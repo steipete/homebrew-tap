f067beda85ad1864ae800ae644ec742cf671f9fd
Peter Steinberger <steipete@gmail.com>
2026-09-13 23:51:54 -0700

    fix(blucli): stringify version in formula test

--- Formula/blucli.rb
+++ Formula/blucli.rb
@@ -31,6 +31,6 @@ class Blucli < Formula
   end
 
   test do
-    assert_match version, shell_output("#{bin}/blu --version")
+    assert_match version.to_s, shell_output("#{bin}/blu --version")
   end
 end
